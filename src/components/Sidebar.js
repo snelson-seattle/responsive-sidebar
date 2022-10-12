4403fa0d0104d6d14a65d5089ef77d5e04e2b4e9
Scott Nelson <snelson@instructors.2u.com>
2022-10-12 10:07:46 -0700

    desktop layout finished

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -2,48 +2,52 @@ import { FaSketch, FaHome, FaSearch, FaStream, FaBook, FaUsers, FaToolbox, FaSho
 
 const Sidebar = () => {
   return (
-    <div className="w-[180px] h-screen bg-gray-900 text-gray-200 flex flex-col items-center">
-      <div className="w-[80%] py-2.5 px-0 mb-2.5 flex items-center bg-blue-600">
+    <div className="w-[180px] h-screen bg-gray-900 text-gray-300 flex flex-col items-center">
+      <div className="w-[80%] py-2.5 px-0 mb-2.5 flex items-center border-b border-solid border-gray-500">
         <FaSketch size="2.5rem" className="mr-2.5 cursor-pointer pl-2.5" />
         <span className="text-2xl font-bold">App</span>
       </div>
-      <div className="w-[80%] py-2.5 px-0 mb-2.5 bg-blue-600">
+      <div className="w-[85%] py-2.5 px-0 mb-2.5 border-b border-solid border-gray-500">
         <ul>
-          <li className="flex justify-start items-center px-2">
-            <FaHome className="" />
+          <li className="flex justify-start items-center px-2 text-xl font-medium mb-2.5 cursor-pointer p-2.5 rounded-[10px] hover:bg-gray-700 hover:text-white active:bg-gray-700 active:text-white">
+            <FaHome className="mr-2 w-[30px]" />
             <span className="list-item-text">Dashboard</span>
           </li>
-          <li className="flex justify-start items-center px-2">
-            <FaSearch />
+          <li className="flex justify-start items-center px-2 text-xl font-medium mb-2.5 cursor-pointer p-2.5 rounded-[10px] hover:bg-gray-700 hover:text-white active:bg-gray-700 active:text-white">
+            <FaSearch className="mr-2 w-[30px]" />
             <span className="list-item-text">Search</span>
           </li>
-          <li className="flex justify-start items-center px-2">
-            <FaStream />
+          <li className="flex justify-start items-center px-2 text-xl font-medium mb-2.5 cursor-pointer p-2.5 rounded-[10px] hover:bg-gray-700 hover:text-white active:bg-gray-700 active:text-white">
+            <FaStream className="mr-2 w-[30px]" />
             <span className="list-item-text">Insights</span>
           </li>
-          <li className="flex justify-start items-center px-2">
-            <FaBook />
+          <li className="flex justify-start items-center px-2 text-xl font-medium mb-2.5 cursor-pointer p-2.5 rounded-[10px] hover:bg-gray-700 hover:text-white active:bg-gray-700 active:text-white">
+            <FaBook className="mr-2 w-[30px]" />
             <span className="list-item-text">Docs</span>
           </li>
-          <li className="flex justify-start items-center px-2">
-            <FaUsers />
+          <li className="flex justify-start items-center px-2 text-xl font-medium mb-2.5 cursor-pointer p-2.5 rounded-[10px] hover:bg-gray-700 hover:text-white active:bg-gray-700 active:text-white">
+            <FaUsers className="mr-2 w-[30px]" />
             <span className="list-item-text">Community</span>
           </li>
-          <li className="flex justify-start items-center px-2">
-            <FaToolbox />
+          <li className="flex justify-start items-center px-2 text-xl font-medium mb-2.5 cursor-pointer p-2.5 rounded-[10px] hover:bg-gray-700 hover:text-white active:bg-gray-700 active:text-white">
+            <FaToolbox className="mr-2 w-[30px]" />
             <span className="list-item-text">Tools</span>
           </li>
-          <li className="flex justify-start items-center px-2">
-            <FaShoppingCart />
+          <li className="flex justify-start items-center px-2 text-xl font-medium mb-2.5 cursor-pointer p-2.5 rounded-[10px] hover:bg-gray-700 hover:text-white active:bg-gray-700 active:text-white">
+            <FaShoppingCart className="mr-2 w-[30px]" />
             <span className="list-item-text">Market</span>
           </li>
-          <li className="flex justify-start items-center px-2">
-            <FaQuestionCircle className="" />
+          <li className="flex justify-start items-center px-2 text-xl font-medium mb-2.5 cursor-pointer p-2.5 rounded-[10px] hover:bg-gray-700 hover:text-white active:bg-gray-700 active:text-white">
+            <FaQuestionCircle className="mr-2 w-[30px]" />
             <span className="list-item-text">Resources</span>
           </li>
         </ul>
       </div>
-      <div className="w-[80%] py-2.5 px-0 mb-2.5 bg-blue-600"></div>
+      <div className="w-[80%] py-2.5 px-0 mb-2.5 flex justify-center items-center">
+        <div className="w-8 h-8 border border-solid border-gray-500 cursor-pointer mr-3 rounded-[5px] bg-gray-900"></div>
+        <div className="w-8 h-8 border border-solid border-gray-500 cursor-pointer mr-3 rounded-[5px] bg-blue-900"></div>
+        <div className="w-8 h-8 border border-solid border-gray-500 cursor-pointer mr-3 rounded-[5px] bg-white"></div>
+      </div>
     </div>
   );
 };
